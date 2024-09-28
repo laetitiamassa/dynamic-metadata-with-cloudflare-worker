f926169c00acd2c5509728ca74ffdafedad8d5d1
Laetitia Massa <laetitiamassa@gmail.com>
2024-09-28 12:02:18 +0200

    Added Origines collection

--- config.js
+++ config.js
@@ -4,6 +4,10 @@ export const config = {
       {
           pattern: "/product/[^/]+",
           metaDataEndpoint: "https://baserow-pinshasa-u21986.vm.elestio.app/api/database/rows/table/560/{row_id}/?user_field_names=true"
+      },
+		{
+          pattern: "/origins/[^/]+",
+          metaDataEndpoint: "https://baserow-pinshasa-u21986.vm.elestio.app/api/database/rows/table/548/{row_id}/?user_field_names=true"
       }
       // Add more patterns and their metadata endpoints as needed
   ]
